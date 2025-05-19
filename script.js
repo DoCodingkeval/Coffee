1acf7b28f81681d49f082ca370f5a49b6338c974
DoCodingkeval <gohelkeval7@gmail.com>
2025-05-19 18:32:39 +0530

    commit

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-/* ==================MENU TOGGLE================= */
-
-let menu = document.querySelector(".header__menu")
-let nav = document.querySelector(".navbar")
-let close = document.querySelector(".close__icon")
-
-menu.addEventListener("click",function(){
-    nav.classList.add("active")
-})
-close.addEventListener("click",function(){
-    nav.classList.remove("active")
-})
 
 const swiper = new Swiper('.testimonials__swiper', {
     loop: true,
@@ -40,15 +28,84 @@ const swiper = new Swiper('.testimonials__swiper', {
   });
 
 /* ==================GSAP================= */
+function PageOneAnim (){
 
-gsap.from(".main__herosection",{
-    scale: 1.1,
+
+gsap.from(".main__leftsection, .main__rightsection",{
+    scale: 1.2,
     opacity: 0,
-    duration: 1
-})
+    duration: 2,
+    delay: 0.6,
+    ease: "power3.out"
+},"same")
 
 gsap.from(".nav__items li",{
     y:30,
     opacity: 0,
+    duration:1,
+    delay: 0.4,
     stagger: 0.2
+},"same")
+
+}
+
+function PageSecondAnim (){
+let timeline = gsap.timeline({
+    scrollTrigger:{
+        trigger: ".aboutsection",
+        scroller: "body",
+        start: "top 50%",
+        end: "20px center"
+    }
 })
+
+timeline.from(".about__image",{
+    x:-50,
+    opacity: 0,
+    duration: 1,
+    ease: "power3.out"
+})
+
+timeline.from(".about__rightsection",{
+    x: 100,
+    opacity: 0
+},"=-1")
+}
+
+function PageThirdAnim (){
+let tl = gsap.timeline({
+    scrollTrigger: {
+        scroller: "body",
+        trigger: ".menusection",
+        start: "top 20%"
+    }
+})
+
+tl.from(".menu__image",{
+    scale: 1.1,
+    opacity: 0
+})
+}
+
+function PagefourAnim (){
+let tltestimonial = gsap.timeline({
+    scrollTrigger: {
+        scroller: "body",
+        trigger: ".testimonialssection",
+        start: "top 20%",
+        end: "top top"
+    }
+})
+
+tltestimonial.from(".testimonials__image",{
+    y:100,
+    opacity:0
+})
+}
+
+if(window.innerWidth > 768){
+    PageOneAnim();
+    PageSecondAnim();
+    PageThirdAnim();
+    PagefourAnim();
+}
